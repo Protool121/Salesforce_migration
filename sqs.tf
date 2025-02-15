22138e59da6062be99b6c2db41a231ad348b615b
Protool121 <yonatan1@hotmail.co.uk>
2025-02-15 18:48:23 +0000

    added VPC and updated SQS setting

--- sqs.tf
+++ sqs.tf
@@ -1,71 +1,71 @@
 
 
-data "aws_s3_bucket" "bucket" {
-  bucket = "salesrunner"
-}
+# data "aws_s3_bucket" "bucket" {
+#   bucket = "salesrunner"
+# }
 
 
 
-resource "aws_sqs_queue" "sqsdev-temp" {
-  name                      = "salesrunner_sqs"
-  delay_seconds             = 90
-  max_message_size          = 2048
-  message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
-  policy = data.aws_iam_policy_document.sns_topic_policy.json
-  tags = {
-    Environment = "Salesrunner"
-  }
-}
+# resource "aws_sqs_queue" "sqsdev-temp" {
+#   name                      = "salesrunner_sqs"
+#   delay_seconds             = 90
+#   max_message_size          = 2048
+#   message_retention_seconds = 86400
+#   receive_wait_time_seconds = 10
+#   policy = data.aws_iam_policy_document.sns_topic_policy.json
+#   tags = {
+#     Environment = "Salesrunner"
+#   }
+# }
 
 
-data "aws_iam_policy_document" "sns_topic_policy" {
-  policy_id = "__default_policy_ID"
+# data "aws_iam_policy_document" "sns_topic_policy" {
+#   policy_id = "__default_policy_ID"
 
-  statement {
-    actions = [
-      "SNS:Subscribe",
-      "SNS:SetTopicAttributes",
-      "SNS:RemovePermission",
-      "SNS:Receive",
-      "SNS:Publish",
-      "SNS:ListSubscriptionsByTopic",
-      "SNS:GetTopicAttributes",
-      "SNS:DeleteTopic",
-      "SNS:AddPermission",
-    ]
-    effect = "Allow"
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-
-
-    sid = "__default_statement_ID"
-  }
-}
+#   statement {
+#     actions = [
+#       "SNS:Subscribe",
+#       "SNS:SetTopicAttributes",
+#       "SNS:RemovePermission",
+#       "SNS:Receive",
+#       "SNS:Publish",
+#       "SNS:ListSubscriptionsByTopic",
+#       "SNS:GetTopicAttributes",
+#       "SNS:DeleteTopic",
+#       "SNS:AddPermission",
+#     ]
+#     effect = "Allow"
+#     principals {
+#       type        = "AWS"
+#       identifiers = ["*"]
+#     }
 
 
-resource "aws_sqs_queue" "queue" {
-  name = "s3-event-notification-queue"
+#     sid = "__default_statement_ID"
+#   }
+# }
 
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "sqs:SendMessage",
-      "Resource": "arn:aws:sqs:*:*:s3-event-notification-queue",
-      "Condition": {
-        "ArnEquals": { "aws:SourceArn": "${data.aws_s3_bucket.bucket.arn}" }
-      }
-    }
-  ]
-}
-POLICY
-}
+
+# resource "aws_sqs_queue" "queue" {
+#   name = "s3-event-notification-queue"
+
+#   policy = <<POLICY
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": "*",
+#       "Action": "sqs:SendMessage",
+#       "Resource": "arn:aws:sqs:*:*:s3-event-notification-queue",
+#       "Condition": {
+#         "ArnEquals": { "aws:SourceArn": "${data.aws_s3_bucket.bucket.arn}" }
+#       }
+#     }
+#   ]
+# }
+# POLICY
+# }
 
 
 # resource "aws_s3_bucket_notification" "bucket_notification" {
---
+++ vpc.tf
@@ -0,0 +1,160 @@
+
+#  Networking setup
+
+resource "aws_vpc" "runner" {
+  cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "runner-vpc"
+  }
+}
+
+# Networking Privsate subnet
+resource "aws_subnet" "private-runner" {
+  vpc_id     = aws_vpc.runner.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "private-runner"
+  }
+}
+
+# Networking Public subnet
+
+resource "aws_subnet" "public-runner" {
+  vpc_id     = aws_vpc.runner.id
+  cidr_block = "10.0.7.0/24"
+
+  tags = {
+    Name = "public-runner"
+  }
+}
+
+# Networking Internet Gateway for public subnet
+
+resource "aws_internet_gateway" "runnergw" {
+  vpc_id = aws_vpc.runner.id
+
+  tags = {
+    Name = "runner-igw"
+  }
+}
+
+# Networking Public subnet route table
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.runner.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.runnergw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+# Networking Privsate subnet route table
+
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.runner.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.runnergw.id
+  }
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+
+# resource "aws_route_table_association" "private" {
+#   subnet_id      = aws_subnet.private-runner.id
+#   route_table_id = aws_route_table.private-route-table.id
+# }
+
+
+# Networking Privsate subnet
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public-runner.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+
+resource "aws_security_group" "runnersg" {
+  name        = "runner-sg"
+  description = "inbound incoming"
+  vpc_id      = aws_vpc.runner.id
+  tags = {
+    Name = "runner-sg"
+  }
+}
+
+
+
+# data "local_file" "user_data" {
+#   filename = "./file.sh"
+# }
+
+
+resource "aws_instance" "runner-ec2" {
+  ami                     = "ami-03a725ae7d906005d"
+  instance_type           = "t2.micro"
+  subnet_id = aws_subnet.public-runner.id
+  associate_public_ip_address = true
+  
+
+  user_data =<<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y httpd
+    systemctl start httpd
+    systemctl enable httpd
+    EC2_AVAIL_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+    echo "Hello World from $(hostname -f) in AZ $EC2_AVAIL_ZONE " > /var/www/html/index.html
+EOF
+# }
+  tags = {
+    Name = "runner-ec2"
+  }
+}
+
+
+# resource "aws_security_group" "runner-ec2-public-sg" {
+#   name        = "runner-ec2-public-ingress-rule"
+#   description = "runner-ec2-public-ingress-rule"
+#   vpc_id      = aws_vpc.runner.id
+#   tags = {
+#     Name = "runner-ec2-public-sg"
+#   }
+# }
+
+# resource "aws_vpc_security_group_ingress_rule" "ingress-sg-ec2" {
+#   security_group_id = aws_security_group.runner-ec2-public-sg.id
+
+#   cidr_ipv4   = "0.0.0.0/0"
+#   from_port   = 80
+#   ip_protocol = "tcp"
+#   to_port     = 80
+
+#   tags = {
+#     Name = "ingress-sg-ec2"
+#   }
+# }
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = aws_vpc.runner.id
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+
+
+
